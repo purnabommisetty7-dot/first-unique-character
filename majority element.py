6c2b66724eb04497938cc9558a5ca6069ce11cf4
purnabommisetty7-dot <purnabommisetty7@gmail.com>
2025-12-27 14:55:28 +0530

    Add files via upload

---
+++ majority element.py
@@ -0,0 +1,15 @@
+def majorityElement(nums):
+    n=len(nums)
+    d={}
+    for i in range (len(nums)):
+        if nums[i]in d:
+            d[nums[i]]+=1
+        else:
+            d[nums[i]]=1
+    for key,value in d.items():
+        if value>n//2:
+            return key
+nums=list(map(int,input().split()))
+print(majorityElement(nums))
+        
+
